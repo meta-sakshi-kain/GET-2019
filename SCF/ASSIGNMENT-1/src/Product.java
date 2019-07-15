50757d309a07ce8b00a9de781006d2328f9fa159
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-07-15 17:13:17 +0530

    Update Product.java

--- SCF/ASSIGNMENT-1/src/Product.java
+++ SCF/ASSIGNMENT-1/src/Product.java
@@ -1,50 +1,47 @@
-
-
 //class product is used to add items in cart
-
 public class Product {
-	int productCode;
-	String itemName;
-	int itemQuantity;
-	double itemPrice;
-	//get the item code from list of items
+int productCode;
+String itemName;
+int itemQuantity;
+double itemPrice;
+//get the item code from list of items
 public int getproductCode() {
-	return productCode;
+return productCode;
 }
 //set the product code in shopping cart
 public void setproductCode(int productCode) {
-	this.productCode = productCode;
+this.productCode = productCode;
 }
 //get the item name from list of items
 public String getItemName() {
-	return itemName;
+return itemName;
 }
 //set the product name in shopping cart
 public void setItemName(String itemName) {
-	this.itemName = itemName;
+this.itemName = itemName;
 }
 //get the available quantity from list of items
 public int getQuantity() {
-	return itemQuantity;
+return itemQuantity;
 }
 //set the product quantity in shopping cart
 public void setQuantity(int itemQuantity) {
-	this.itemQuantity = itemQuantity;
+this.itemQuantity = itemQuantity;
 }
 //get the item price from list of items
 public double getPrice() {
-	return itemPrice;
+return itemPrice;
 }
 //set the product price in shopping cart
 public void setPrice(double itemPrice) {
-	this.itemPrice = itemPrice;
+this.itemPrice = itemPrice;
 }
 //constructor
-	public Product(int productCode, String itemName, int itemQuantity, double itemPrice) {
-		super();
-		this.productCode = productCode;
-		this.itemName = itemName;
-		this.itemQuantity = itemQuantity;
-		this.itemPrice = itemPrice;
-	}
+public Product(int productCode, String itemName, int itemQuantity, double itemPrice) {
+super();
+this.productCode = productCode;
+this.itemName = itemName;
+this.itemQuantity = itemQuantity;
+this.itemPrice = itemPrice;
+}
 }

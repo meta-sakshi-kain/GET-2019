6f80c4775ec507d46538c6060367ecb43e0c330e
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-07-20 21:13:57 +0530

    Update MathematicalOperations.java

--- ASSIGNMENT-5/src/MathematicalOperations.java
+++ ASSIGNMENT-5/src/MathematicalOperations.java
@@ -1,18 +1,27 @@
 
 public class MathematicalOperations {
-	public static int lcm(int num1,int num2){
-	
-		return num1*num2/hcf(num1,num2);
-	}
-	public static int hcf(int num1,int num2){
-		
-		if(num1==0)
-			return num2;
-		else if(num2==0)
-			return num1;
-		else
-			return hcf(num2%num1,num1);
-		
-	}
+/**
+ * @param num1 represents first number
+ * @param num2 represents second number
+ * @return lcm of numbers
+ */
+public static int lcm(int num1,int num2){
 
+	return num1*num2/hcf(num1,num2);
 }
+/**
+ * @param num1 represents first number
+ * @param num2 represents second number
+ * @return hcf of the numbers
+ */
+public static int hcf(int num1,int num2){
+
+if(num1==0)
+	return num2;
+else if(num2==0)
+	return num1;
+else
+	return hcf(num2%num1,num1);
+}
+}
+

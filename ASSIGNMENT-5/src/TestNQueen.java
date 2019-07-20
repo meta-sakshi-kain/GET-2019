4c12cc812f1a86693b04061f9f41d048960fd5ff
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-07-20 21:18:55 +0530

    Update TestNQueen.java

--- ASSIGNMENT-5/src/TestNQueen.java
+++ ASSIGNMENT-5/src/TestNQueen.java
@@ -2,26 +2,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class TestNQueen {
 
-	@Test
-	public void testWhenSolExist() {
-		NQueen Queen = new NQueen(); 
-		int dimensionOfMatrix = 4;
-		int dimensionOfMatrix1 = 8;
-		int [][] board = new int[dimensionOfMatrix][dimensionOfMatrix];
-		assertTrue(Queen.nQueen(board,0,dimensionOfMatrix));
-		int [][] board1 = new int[dimensionOfMatrix1][dimensionOfMatrix1];
-		assertTrue(Queen.nQueen(board1,0,dimensionOfMatrix1));
-		
-	}
-	@Test
-	public void testWhenSolNotExist() {
-		NQueen Queen = new NQueen(); 
-		int dimensionOfMatrix = 3;
-		int [][] board = new int[dimensionOfMatrix][dimensionOfMatrix];
-		assertFalse(Queen.nQueen(board,0,dimensionOfMatrix));
-	}
-	
+@Test
+public void testWhenSolExist() {
+	NQueen Queen = new NQueen(); 
+	int dimensionOfMatrix = 4;
+	int dimensionOfMatrix1 = 8;
+	int dimensionOfMatrix2 = 16;
+	int [][] board = new int[dimensionOfMatrix][dimensionOfMatrix];
+	assertTrue(Queen.nQueen(board,0,dimensionOfMatrix));
+	System.out.println();
+	int [][] board1 = new int[dimensionOfMatrix1][dimensionOfMatrix1];
+	assertTrue(Queen.nQueen(board1,0,dimensionOfMatrix1));
+	System.out.println();
+	int [][] board2 = new int[dimensionOfMatrix2][dimensionOfMatrix2];
+	assertTrue(Queen.nQueen(board2,0,dimensionOfMatrix2));
+
 }
+@Test
+public void testWhenSolNotExist() {
+	NQueen Queen = new NQueen(); 
+	int dimensionOfMatrix = 3;
+	int [][] board = new int[dimensionOfMatrix][dimensionOfMatrix];
+	assertFalse(Queen.nQueen(board,0,dimensionOfMatrix));
+}
+
+}
+

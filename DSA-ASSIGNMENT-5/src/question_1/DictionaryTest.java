3ae289426b8fccc9e2ce136729bb6d722842422b
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-06 12:12:04 +0530

    Update DictionaryTest.java

--- DSA-ASSIGNMENT-5/src/question_1/DictionaryTest.java
+++ DSA-ASSIGNMENT-5/src/question_1/DictionaryTest.java
@@ -8,30 +8,38 @@ import org.junit.Test;
 
 import question_1.DataEntry;
 import question_1.Dictionary;
-//test class to check operations on dictionary
+//test class to test operations on dictionary
 public class DictionaryTest {
-       //test case the list is sorted on the basis of key
+
+	//test case the list is sorted on the basis of key
 	@Test
 	public void sortingTest() {
-			Dictionary d = new Dictionary("file.json");
+		Dictionary d = new Dictionary("file.json");
 		List<DataEntry> list = new ArrayList<DataEntry>();
 		d.addData(33, "huma");
 		d.addData(33, "huma");
 		list = d.getSortedList();
 		assertEquals("piyush",d.getValue(1));
-		}
 	}
-        //test case to check value at a given key
+	//test case to check value at a given key
 	@Test
 	public void getValueTest() {
 		Dictionary d = new Dictionary("file.json");
 		assertEquals("vrinda",d.getValue(6));
 	}
-        //test case when key not found
+	//test case when key not found
 	@Test
 	public void keyNotFoundTest() {
 		Dictionary d = new Dictionary("file.json");
 		assertEquals("key not found",d.getValue(277));
 	}
-	
+	//test case to remove data from dictionary
+	@Test
+	public void removeDataTest() {
+		Dictionary d = new Dictionary("file.json");
+		List<DataEntry> list = new ArrayList<DataEntry>();
+		d.removeData(4);
+		list = d.getSortedList();
+		assertEquals(4, list.size());
+	}
 }

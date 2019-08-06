43d0535c0de777999e75f79e6c5af0cb501f66a8
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-06 11:51:09 +0530

    Update EmployeeLinkedListTest.java

--- DSA-ASSIGNMENT-5/src/question_2/EmployeeLinkedListTest.java
+++ DSA-ASSIGNMENT-5/src/question_2/EmployeeLinkedListTest.java
@@ -1,5 +1,7 @@
 package question_2;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 //test class for employee class
 public class EmployeeLinkedListTest {
@@ -10,9 +12,11 @@ public class EmployeeLinkedListTest {
 		EmployeeLinkedList list = new EmployeeLinkedList();
 	    list.insertEmployee(new Employee("Sakshi Kain",22 , 22000));
 	    list.insertEmployee(new Employee("Diksha Kain", 24, 20000));
-	    list.insertEmployee(new Employee("Raksha Kardam", 18, 5000));
+	    list.insertEmployee(new Employee("Raksha Kardam", 18, 55000));
 	    InsertionSort sort = new InsertionSort();
-	    sort.sort(list,list.size);
-	    list.print();
+	    String[] expected=new String[]{"Raksha Kardam","Sakshi Kain","Diksha Kain"};
+	    list= sort.sort(list,list.size);
+	    String[] result=list.result();
+	    assertArrayEquals(expected,result);
 	}
 }

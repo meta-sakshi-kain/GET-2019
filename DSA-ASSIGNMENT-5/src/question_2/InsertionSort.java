6279105873230f9c147640c8353858464bade8d6
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-08 17:08:15 +0530

    Update InsertionSort.java

--- DSA-ASSIGNMENT-5/src/question_2/InsertionSort.java
+++ DSA-ASSIGNMENT-5/src/question_2/InsertionSort.java
@@ -2,70 +2,71 @@ package question_2;
 //class to implement insertion sort on linked list
 public class InsertionSort {
 	//prev node to get previous node of current
-	//current node to point current node in sorted list
-	//compare node to point to node to be compared
-	//end node to point ending node of sorted list 
-	//make end node as starting node initially
-	private Node prev,current,compare,end;
-	/**
-	 * @param list to be sorted
-	 * @param size of list
-	 * @return
-	 */
-	//method to sort empployees on the basis of salary
-	public EmployeeLinkedList sort(EmployeeLinkedList list,int size) {
-		
-		end = list.head.next;
-		//outer loop 
-		for(int i = 1; i<size; i++) {
-			//previous = head
-		    prev = list.head;
-		    //current = first element of list
-			current =  list.head.next;
-			//inner loop
-			for(int j = 0; j<i; j++) {
-				//node to be compared
-		        compare = end.next;
-		        //if salary is less no need to insert that element in sorted list
-				if(compare.employee.getSalary() < current.employee.getSalary()) {
-					//shift compare and previous
-					caseOfNotInserting();
-				}
-				//else put element in sorted list
-				else if(compare.employee.getSalary() > current.employee.getSalary()) {
-					caseOfInserting(list);
+//current node to point current node in sorted list
+//compare node to point to node to be compared
+//end node to point ending node of sorted list 
+//make end node as starting node initially
+private Node prev,current,compare,end;
+/**
+ * @param list to be sorted
+ * @param size of list
+ * @return
+ */
+//method to sort empployees on the basis of salary
+public EmployeeLinkedList sort(EmployeeLinkedList list,int size) {
+   end = list.head.next;
+	//outer loop 
+	for(int i = 1; i<size; i++) {
+		//previous = head
+		prev = list.head;
+		//current = first element of list
+		current =  list.head.next;
+		//inner loop
+		for(int j = 0; j<i; j++) {
+			//node to be compared
+			compare = end.next;
+			//if salary is less no need to insert that element in sorted list
+			if(compare.employee.getSalary() < current.employee.getSalary()) {
+				//shift compare and previous
+				caseOfNotInserting(list);
+				if(j == i-1)
+					end = end.next;
+
+			}
+			//else put element in sorted list
+			else if(compare.employee.getSalary() > current.employee.getSalary()) {
+				caseOfInserting(list);
+				break;
+			}
+			//if salary of both are equal
+			else {
+				if(compare.employee.getAge() > current.employee.getAge()) {
+					caseOfNotInserting(list);
+					if(j == i-1)
+					end = end.next;
+                               }
+				else{
+				 caseOfInserting(list);
 					break;
 				}
-				//if salary of both are equal
-				else {
-					if(compare.employee.getAge() > current.employee.getAge()) {
-						caseOfNotInserting();
-					}
-					else{
-						caseOfInserting(list);
-						break;
-					}
-				}
 			}
-			list.last = end;
-			end = end.next;
 		}
-		return list;
-		
+		list.last = end;
+		//end = end.next;
 	}
-	/**
-	 * when element does not need to be inserted
-	 */
-	//when the node is already sorted in the list
-	private void caseOfNotInserting(){
-		current = current.next;
-		prev = prev.next;
-	}
-	/**
-	 * method when element need to be inserted in sorted list
-	 * @param list 
-	 */
-	//when the element to be added in sorted list
+	return list;
+}
+/**
+ * when element does not need to be inserted
+ */
+private void caseOfNotInserting(EmployeeLinkedList list){
+	current = current.next;
+	prev = prev.next;
+}
+/**
+ * method when element need to be inserted in sorted list
+ * @param list 
+ */
 	private void caseOfInserting(EmployeeLinkedList list){
 		if(prev==list.head) {
 			list.head.next = compare;
@@ -76,3 +77,4 @@ public class InsertionSort {
 		end.next = temp;
 	}
 }
+

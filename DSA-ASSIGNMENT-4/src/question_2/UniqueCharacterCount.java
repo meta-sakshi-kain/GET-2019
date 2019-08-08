736a1f5bfdaf6c9e79a573ef92c441aa980011f1
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-08 11:24:06 +0530

    Update UniqueCharacterCount.java

--- DSA-ASSIGNMENT-4/src/question_2/UniqueCharacterCount.java
+++ DSA-ASSIGNMENT-4/src/question_2/UniqueCharacterCount.java
@@ -1,28 +1,41 @@
-package question_1;
+package question_2;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-//class to add employee
-public class EmployeeCollection {
-	
-	//collection of employee made using a set
-	 ArrayList<Employee> emp=new ArrayList<Employee>();
-	Set<Integer> empID=new HashSet<Integer>();
+
+public class UniqueCharacterCount {
+    //cache map to store user entered string 
+	private Map<String, Integer> map = new HashMap<String, Integer>();
+	//storing unique characters
+	private Set<Character> set = new HashSet<Character>();
+
 	/**
-	 * method adds employee to set
-	 * @param empId of employee
-	 * @param name of employee
-	 * @param address of employee
+	 * @param str string for unique characters count
+	 * @return number of unique character in str string
 	 */
-	//method to add employee 
-	public void addEmployee(int empId, String name, String address){
-		
-		Employee employee = new Employee(empId, name, address);
-		//if id is unique add employee to set
-		if(empID.add(empId)){
-			emp.add(employee);
+	//function to calculate unique characters from string
+	public int countUniqueCharacters(String str) {
+		str = str.toLowerCase();
+		// check if string previously encountered
+		if (map.keySet().contains(str))
+			// return previous value
+			return map.get(str);
+		// for a new string
+		for (int i = 0; i < str.length(); i++) {
+			//Ignore spaces
+			if(str.charAt(i) ==' ')
+				i++;
+			// check if character is already exist in set
+			if (!set.add(str.charAt(i)))
+				// remove character from set
+				set.remove(str.charAt(i));
 		}
+		// put string in map with number of unique characters
+		map.put(str, set.size());
+		// return number of unique characters
+		return set.size();
 	}
 }
 

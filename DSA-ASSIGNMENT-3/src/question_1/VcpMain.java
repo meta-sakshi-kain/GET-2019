23f67132031d32560a927ab5621a3262d0b3f617
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-09 15:48:02 +0530

    Add files via upload

---
+++ DSA-ASSIGNMENT-3/src/question_1/VcpMain.java
@@ -0,0 +1,19 @@
+package question_1;
+
+import java.util.Scanner;
+
+//main class to execute commands on command prompt
+public class VcpMain {
+	public static void main(String[] arg) {
+		VirtualCommandPrompt v = new VirtualCommandPrompt();
+		System.out.print(v.getRoot().getName() + ">" + "\t");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		while (input != "exit") {
+			String result = v.vcp(input);
+			System.out.print(result + ">\t");
+			Scanner scanner1 = new Scanner(System.in);
+			input = scanner1.nextLine();
+		}
+	}
+}

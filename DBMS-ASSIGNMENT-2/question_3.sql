6cee5574aa7dfbb805b058331f197eaa8ecd0c47
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-19 16:22:46 +0530

    Update question_3.sql

--- DBMS-ASSIGNMENT-2/question_3.sql
+++ DBMS-ASSIGNMENT-2/question_3.sql
@@ -1,6 +1,6 @@
 USE STOREFRONT;
 
-
+# Recent 50 Orders placed (Id, Order Date, Order Total).
 SELECT 
     ORDER_ID, ORDER_DATE, AMOUNT
 FROM
@@ -8,7 +8,7 @@ FROM
 ORDER BY ORDER_ID DESC
 LIMIT 50;
 
-
+#10 most expensive Orders.
 SELECT 
     ORDER_ID, ORDER_DATE, AMOUNT
 FROM
@@ -16,7 +16,8 @@ FROM
 ORDER BY AMOUNT DESC
 LIMIT 10;
 
-
+#all the Orders which are placed more than 10 days old and one or
+# more items from those orders are still not shipped.
 SELECT 
     ORDER_ID, ORDER_DATE, AMOUNT
 FROM
@@ -24,7 +25,7 @@ FROM
 WHERE
     DATEDIFF(CURDATE(), ORDER_DATE) >= 10 AND order_status = 'Not Shipped';
 
-
+#list of shoppers which haven't ordered anything since last month.
 SELECT 
     SHOPPER.SHOPPER_NAME
 FROM
@@ -33,7 +34,7 @@ FROM
 WHERE
     SHOPPER.SHOPPER_ID = ORDERS.SHOPPER_ID AND DATEDIFF(CURDATE(), ORDER_DATE) >= 30;
 
-
+#list of shopper along with orders placed by them in last 15 days. 
 SELECT 
     SHOPPER.SHOPPER_NAME,
     ORDERS.order_id,
@@ -45,7 +46,7 @@ FROM
 WHERE
     SHOPPER.SHOPPER_ID = ORDERS.SHOPPER_ID AND DATEDIFF(CURDATE(), ORDER_DATE) <= 15;
  
- 
+#list of order items which are in “shipped” state for particular Order Id 
 SELECT 
     PRODUCT_NAME
 FROM
@@ -55,7 +56,7 @@ FROM
 WHERE
     ORDERS.ORDER_ID = 1 AND ORDERS.ORDER_STATUS = 'Shipped'; 
   
-
+#list of order items along with order placed date which fall between Rs 20000 to Rs 50000 price.
 SELECT 
     ITEMS_ORDERED.PRODUCT_NAME, ORDERS.ORDER_DATE
 FROM

a3f47d93fb58d72a7b6ff62cf07c56998f7e8f8a
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-19 19:03:09 +0530

    Add files via upload

---
+++ DBMS-ASSIGNEMNT-3/question_5.sql
@@ -0,0 +1,44 @@
+/*Create a view displaying the order information (Id, Title, Price, Shopper’s name, Email, Orderdate, Status) with 
+latest ordered items should be displayed first for last 60 days.*/
+CREATE OR REPLACE VIEW order_information AS
+    SELECT DISTINCT
+        o.order_id,
+        p.product_id,
+        p.name AS product_name,
+        c.product_quantity,
+        c.total_amount AS price,
+        u.name AS shopper_name,
+        u.email,
+        o.order_date AS date_of_order,
+        o.order_status
+    FROM
+        orders o,
+        user u,
+        shopper s,
+        product p,
+       items_ordered c
+    WHERE
+        p.product_id = c.product_id
+            AND c.shopper_id = s.shopper_id
+            AND s.email = u.email
+            AND o.shopper_id = c.shopper_id
+            AND o.amount = c.total_amount
+            AND DATEDIFF(CURDATE(),order_date) <  60
+    ORDER BY o.order_date DESC;
+    
+    
+#Use the above view to display the Products(Items) which are in ‘shipped’ state.
+SELECT 
+    product_id,order_id
+FROM
+    order_information
+WHERE
+    order_status = 'Shipped';
+    
+#Use the above view to display the top 5 most selling products.
+SELECT 
+    product_id, COUNT(product_id)
+FROM
+    order_information
+GROUP BY product_id
+ORDER BY COUNT(product_id) DESC;

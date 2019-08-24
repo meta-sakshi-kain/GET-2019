c9395c049a2b8da20131dd9f5b9eb228e94a21b9
meta-sakshi-kain <52689016+meta-sakshi-kain@users.noreply.github.com>
2019-08-25 00:18:16 +0530

    Add files via upload

---
+++ DBMS-ASSIGNEMNT-4/question_1.sql
@@ -0,0 +1,49 @@
+USE StoreFront;
+
+/*
+*display all the table names present in StoreFront.
+*/
+SHOW TABLES;
+
+ALTER TABLE CategoryProductLink DROP FOREIGN KEY Product_Id1;
+
+ALTER TABLE stock DROP FOREIGN KEY Product_Id2;
+
+ALTER TABLE items_ordered DROP FOREIGN KEY product_id3;
+
+ALTER TABLE images DROP FOREIGN KEY Product_Id4;
+
+/*
+*remove Product table of the StoreFront database.
+*/
+DROP TABLE Product;
+
+/*
+*Create the Product table again.
+*/
+CREATE TABLE Product(
+    product_id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    quantity INT NOT NULL,
+    price DOUBLE NOT NULL,
+    category_id INT,
+    date DATE NOT NULL,
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id)
+        REFERENCES Category (category_id)
+);
+
+
+ALTER TABLE stock
+   ADD CONSTRAINT Product_Id1 FOREIGN KEY ( product_Id) REFERENCES Product( product_Id);
+
+ALTER TABLE stock
+   ADD CONSTRAINT Product_Id2 FOREIGN KEY ( product_Id) REFERENCES Product( product_Id);
+
+
+ALTER TABLE items_ordered 
+   ADD CONSTRAINT product_id3 FOREIGN KEY (product_id) REFERENCES Product (product_id);
+   
+   
+ALTER TABLE images
+   ADD CONSTRAINT Product_Id4 FOREIGN KEY ( product_Id) REFERENCES Product( product_Id);
---
+++ DBMS-ASSIGNEMNT-4/question_2.sql
@@ -0,0 +1,93 @@
+USE StoreFront;
+
+/*
+*products which are Active and recently added products should be at top.
+*/
+SELECT 
+    PRODUCT.PRODUCT_ID,
+    PRODUCT.NAME AS PRODUCT_TITLE,
+    CATEGORY.NAME AS CATEGORY_TITLE,
+    PRODUCT.PRICE
+FROM
+    PRODUCT,
+    CATEGORY
+WHERE
+    PRODUCT.CATEGORY_ID = CATEGORY.CATEGORY_ID
+ORDER BY PRODUCT.PRODUCT_ID DESC ;
+
+
+/*
+*the list of products which don't have any images.
+*/
+SELECT 
+    NAME AS PRODUCT_TITLE
+FROM
+    PRODUCT
+WHERE
+    product.product_id NOT IN (SELECT DISTINCT
+        product_id
+    FROM
+        images);
+
+
+/*
+*Display all Id, Title and Parent Category Title for all the Categories listed, 
+*sorted by Parent Category Title and then Category Title.
+*If Category is top category then Parent Category Title column should display “Top Category” as value
+*/
+SELECT 
+    Category1.CATEGORY_ID AS ID,
+    Category1.NAME AS CATEGORY_TITLE,
+    IFNULL(Category2.NAME, 'Top Category') AS PARENT_CATEGORY
+FROM
+    CATEGORY Category1
+        LEFT JOIN
+    CATEGORY Category2 ON Category1.PARENT_CATEGORY = Category2.CATEGORY_ID
+ORDER BY Category2.name,Category1.NAME;
+
+
+/*
+*categories which are not parent of any other category
+*/
+SELECT 
+    Category1.CATEGORY_ID AS ID,
+    Category1.NAME AS CATEGORY_TITLE,
+    Category2.NAME AS PARENT_CATEGORY
+FROM
+    CATEGORY Category1
+        LEFT JOIN
+    CATEGORY Category2 ON Category1.PARENT_CATEGORY = Category2.CATEGORY_ID
+WHERE
+    Category1.CATEGORY_ID NOT IN (SELECT 
+        PARENT_CATEGORY
+    FROM
+        CATEGORY
+    WHERE
+        PARENT_CATEGORY IS NOT NULL);
+        
+        
+/*
+*Product Title, Price & Description which falls into particular category Title (i.e. “Mobile”)
+*/
+SELECT 
+    product.PRODUCT_ID AS ID,
+    product.PRICE,
+    product.NAME AS PRODUCT_TITLE,
+    Category.name AS CATEGORY_NAME
+FROM
+    PRODUCT product
+        LEFT JOIN
+    CATEGORY Category ON Category.CATEGORY_ID = product.CATEGORY_ID
+WHERE
+    Category.NAME IN ('MOBILE') ;
+    
+    
+/*
+*the list of Products whose Quantity on hand (Inventory) is under 50.
+*/
+SELECT 
+    NAME
+FROM
+    PRODUCT
+WHERE
+    QUANTITY <= 50;
